2e95eaf127e21c790bbfc43436d51e4a7e56a906
Vasiliy <vasi1ek745@gmail.com>
2021-08-27 13:52:34 +0500

    add rebro

--- coin.rb
+++ coin.rb
@@ -1,12 +1,16 @@
-puts "Орел или решка?"
+puts "Орел или решка?(очень редко может выпсть ребро)"
 sleep(1)
 answer = STDIN.gets.chomp
 puts "Подбрасывем монетку"
 sleep(1)
 pc = ["орел","решка"].sample
-puts = puts "Выпал #{pc}"
-if pc == answer.downcase
-  puts "ты угадал!"
-else
-  puts "ты проиграл!"
+if rand(6)==1
+  puts  "Решка!"
+  else
+  puts = puts "Выпал #{pc}"
+  if pc == answer.downcase
+    puts "ты угадал!"
+  else
+    puts "ты проиграл!"
+  end
 end
