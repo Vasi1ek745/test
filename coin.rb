08ad0690a66fcf885fd403881fa12cf20a3c0213
Vasiliy <vasi1ek745@gmail.com>
2021-08-27 12:48:10 +0500

    add if else

--- coin.rb
+++ coin.rb
@@ -1,5 +1,12 @@
 puts "Орел или решка?"
 sleep(1)
+answer = STDIN.gets.chomp
 puts "Подбрасывем монетку"
 sleep(1)
-puts "Выпал #{["орел","решка"].sample}"
+pc = ["орел","решка"].sample
+puts = puts "Выпал #{pc}"
+if pc == answer.downcase
+  puts "ты угадал!"
+else
+  puts "ты проиграл!"
+end
